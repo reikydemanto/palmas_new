dadf10c22ae155435062d61ed0564ba665eb6807
reikydemanto <reikydemanto@gmail.com>
2024-11-06 13:32:00 +0700

    cool box done

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -1,4 +1,5 @@
 <div class="container-fluid overflow-hidden p-0">
     @include('templates.navbar')
     @include('templates.bigCarrousel')
+    @include('templates.cool_box')
 </div>
---
+++ resources/views/templates/cool_box.blade.php
@@ -0,0 +1,36 @@
+<style>
+    @media (max-width: 1200px) {
+        .cool-box-img{
+            bottom: -80px!important;
+            width: 60%!important;
+        }
+
+        .font-cool-box-big{
+            font-size: 8vw!important;
+        }
+        .font-cool-box-small{
+            font-size: 2vw!important;
+        }
+    }
+    @media (max-width: 576px) {
+        .cool-box-img{
+            bottom: -40px!important;
+            width: 70%!important;
+        }
+
+        .font-cool-box-big{
+            font-size: 8vw!important;
+        }
+        .font-cool-box-small{
+            font-size: 2vw!important;
+        }
+    }
+</style>
+<section class="container-fluid mt-5 z-0">
+    <h1 class="text-uppercase text-center p-1 font-cool-box-big" style="font-family: 'Helvetica-light';font-size: 5vw;">Is this
+        softdrink?</h1>
+    <h1 class="text-uppercase text-center p-1 font-cool-box-small" style="font-family: 'Helvetica-medium';font-size: 1vw;">this is
+        another experience of drinking games</h1>
+    <img class="position-relative start-50 translate-middle-x z-0 cool-box-img" style="width:45%; bottom: -140px;"
+        src="./images/Cool_Box.png">
+</section>
